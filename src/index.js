58f51d40905e616dc48cf23f0083437894b4c8f5
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-10-22 08:50:34 +0300

    Completed Assignment || Added more features

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     li.append(`${textInput.value} `)
     li.append(btnDel)
+    btnDel.style.marginLeft = '1em'
     tasks.append(li)
 
     textInput.value = '' //after submit, the input box will be removed
@@ -28,9 +29,54 @@ document.addEventListener("DOMContentLoaded", () => {
 
       e.preventDefault()
 
+
+    })
+
+
+    const dropDown = document.createElement('select')
+    dropDown.style.marginLeft = '1em'
+    li.append(dropDown)
+
+    const high = document.createElement('option')
+    high.innerText = 'High'
+    high.value = 'high'
+
+    const medium = document.createElement('option')
+    medium.innerText = 'Medium'
+    medium.value = 'medium'
+
+
+    const low = document.createElement('option')
+    low.innerText = 'Low'   
+    low.value = 'low'
+
+
+
+
+    dropDown.ariaLabel = "Books nad Snippets"
+      const subGroup = document.createElement('optgroup')
+        dropDown.append(subGroup)
+        subGroup.label = 'Select importance'
+
+
+dropDown.append(high)
+dropDown.append(medium)
+dropDown.append(low)
+
+    dropDown.addEventListener('change', (e) => {
+      if(e.target.value === "high"){
+      li.style.color = 'red'
+      }else if(e.target.value === "medium"){
+      li.style.color = 'orange'
+      }else if(e.target.value === "low"){
+      li.style.color = 'green'
+      }
+      console.log(e.target)
     })
 
   })
 
+
+  
 
 });
